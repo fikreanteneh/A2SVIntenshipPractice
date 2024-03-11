6ea48e0f7768e6b97830a7fb0d85786f040b07ea
fikreanteneh <fikre.anteneh01@gmail.com>
2024-03-11 14:10:51 +0300

    FIx Task 2

--- 02.WordCounter/main.go
+++ 02.WordCounter/main.go
@@ -1,71 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
-
-func WordCounter(s string) int {
-	var counter = make(map[string]int)
-	for _, word := range s.Fields(s) {
-
-	}
-
-
+func wordCounter(s string) map[string]int {
+    var counter = make(map[string]int)
+    for _, letter := range s {
+        if !unicode.IsLetter(letter) {
+            continue
+        }
+        counter[strings.ToLower(string(letter))]++
+    }
+    return counter
 }
 
-
-
-func main(){
+func main() {
+	fmt.Println(wordCounter("Hello, World!"))
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
