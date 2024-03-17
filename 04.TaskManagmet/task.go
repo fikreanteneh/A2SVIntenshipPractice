00a2ce782115b084469300ae99e84914cf6c3755
fikreanteneh <fikre.anteneh01@gmail.com>
2024-03-17 22:49:40 +0300

    Bug Fix

--- 04.TaskManagmet/task.go
+++ 04.TaskManagmet/task.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
@@ -9,14 +10,22 @@ import (
 
 
 type Task struct {
-	id          int         `json:id`
-	title       string      `json:"title"`
-	description string      `json:"description"`
-	dueDate     time.Time   `json:"dueDate"`
-	status      bool        `json:"status"`
+	Id          int         `json:"id"`
+	Title       string      `json:"title"`
+	Description string      `json:"description"`
+	DueDate     time.Time   `json:"dueDate"`
+	Status      bool        `json:"status"`
+}
+
+type CreateTaskType struct {
+    Title       string    `json:"title"`
+    Description string    `json:"description"`
+    DueDate     time.Time `json:"dueDate"`
+    Status      bool      `json:"status"`
 }
 
 var db = make(map[int]Task)
+var dbid = 1;
 
 func GetTask(context *gin.Context) {
 	context.JSON(200, db)
@@ -24,37 +33,61 @@ func GetTask(context *gin.Context) {
 
 func GetTaskById(context *gin.Context) {
 	var id, err = strconv.Atoi(context.Param("id"))
-	if err != nil {
-		context.JSON(400, gin.H{"error": "Invalid ID"})
+	var _, ok = db[id]
+		fmt.Println(id, ok)
+
+	if err != nil || !ok {
+		context.JSON(400, "Invalid Id")
 		return
 	}
 	context.JSON(200, db[id])
 }
 
 func CreateTask(context *gin.Context) {
-	var task Task
-	context.BindJSON(&task)
-	task.id = len(db)
-	db[task.id] = task
-	context.JSON(200, db)
+	var task CreateTaskType
+    if err := context.Bind(&task); err != nil {
+        context.JSON(400, "Invalid Request Type")
+        return
+    }
+	var created = Task{
+		Id: dbid,
+		Title: task.Title,
+		Description: task.Description,
+		Status: task.Status,
+		DueDate: task.DueDate,
+	}
+	db[created.Id] = created
+	dbid++
+	context.JSON(201, "Successfully Added task")
 }
+
 
 func UpdateTask(context *gin.Context) {
 	var id, err = strconv.Atoi(context.Param("id"))
-	if err != nil {
-		context.JSON(400, gin.H{"error": "Invalid ID"})
+	var _, ok = db[id]	
+
+	if err != nil || !ok {
+		context.JSON(400, "Invalid Id")
 		return
 	}
-	var task Task
+	var task CreateTaskType
 	context.BindJSON(&task)
-	db[id] = task
-	context.JSON(200, db)
+	db[id] = Task{
+		Id: id,
+		Title: task.Title,
+		Description: task.Description,
+		Status: task.Status,
+		DueDate: task.DueDate,
+	}
+	context.JSON(200, "Successfully Updated Task")
 }
 
 func DeleteTask(context *gin.Context) {
 	var id, err = strconv.Atoi(context.Param("id"))
-	if err != nil {
-		context.JSON(400, gin.H{"error": "Invalid ID"})
+	var _, ok = db[id]
+	fmt.Println(id, ok)
+	if err != nil || !ok {
+		context.JSON(400, "Invalid Id")
 		return
 	}
 	delete(db, id)
